2842353cd3c43725bb5c277aa729bee9faeb97d8
Abhishekdhal <abhishekdhalofficial@gmail.com>
2025-10-28 20:39:21 +0530

    models

--- models/Feedback.js
+++ models/Feedback.js
@@ -5,10 +5,6 @@ const feedbackSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  email: {
-    type: String,
-    required: true
-  },
   message: {
     type: String,
     required: true
